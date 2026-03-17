4bb2c1d05b5049a46aa4cf7e6528f5a4289deca9
unknown <popov.marjan@gmail.com>
2026-03-17 16:34:15 +0100

    Add form for creating post address

--- task-1_frontend-angular/src/app/app.routes.ts
+++ task-1_frontend-angular/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 
+import { AddressInputDemo } from './demo/address-input-demo/address-input-demo';
 import { CardDemo } from './demo/card-demo/card-demo';
 import { NumberInputDemo } from './demo/number-input-demo/number-input-demo';
 import { SelectDemo } from './demo/select-demo/select-demo';
@@ -9,22 +10,27 @@ export const routes: Routes = [
   {
     path: 'select',
     component: SelectDemo,
-    data: { title: 'Auswahl' },
+    data: { section: 'parts', title: 'Auswahl' },
   },
   {
     path: 'card',
     component: CardDemo,
-    data: { title: 'Karte' },
+    data: { section: 'parts', title: 'Karte' },
   },
   {
     path: 'text-input',
     component: TextInputDemo,
-    data: { title: 'Texteingabe' },
+    data: { section: 'parts', title: 'Texteingabe' },
   },
   {
     path: 'number-input',
     component: NumberInputDemo,
-    data: { title: 'Zahleingabe' },
+    data: { section: 'parts', title: 'Zahleingabe' },
+  },
+  {
+    path: 'address-input',
+    component: AddressInputDemo,
+    data: { section: 'combined', title: 'Postadresse' },
   },
   { path: '**', redirectTo: '/card' },
 ];
